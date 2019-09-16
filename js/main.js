5556c6b694130da9e91907a659001d1572494e3a
Ibrahim Olufowora <ioolofowora@sidmach.com>
2019-09-16 16:53:38 +0100

    Update service worker to cache first

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // Make sure sw are supported
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('../sw_cached_pages.js')
-      .then(reg => console.log('Service Worker: Registered (Pages)'))
-      .catch(err => console.log(`Service Worker: Error: ${err}`));
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker
+//       .register('../sw_cached_pages.js')
+//       .then(reg => console.log('Service Worker: Registered (Pages)'))
+//       .catch(err => console.log(`Service Worker: Error: ${err}`));
+//   });
+// }
 
 $(function() {
   const d = new Date();
